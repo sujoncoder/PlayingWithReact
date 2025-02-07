2044af2a4c8a3676314614e9b3746617cbe6b399
sujoncoder <sujonsheikh.in@gmail.com>
2025-02-06 20:59:05 -0800

    done accordian

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -1,28 +1,57 @@
+import { useState } from "react"
 import MaxWidth from "../MaxWidth"
 import AppTitle from "../AppTitle"
 import faqData from "../../utils/AccordianData"
-import { useState } from "react"
+import { Power } from 'lucide-react';
 
 
 const Accordian = () => {
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+    const [multiple, setMultiple] = useState([]);
 
 
-
-    // HANDLE ACCORDIAN SINGLE CLICK
+    // HANDLE SINGLE ACCORDIAN CLICK
     const handleSingleClick = (getId) => {
-        setSelected(getId === selected ? null : getId)
+        setSelected(getId === selected ? null : getId);
     };
+
+    // HANDLE MULTIPLE SELECT CLICK
+    const handleMultipleSelect = (getId) => {
+        let copyMultiple = [...multiple];
+        const findCurrentIndexId = copyMultiple.indexOf(getId)
+
+        if (findCurrentIndexId === -1) copyMultiple.push(getId)
+        else copyMultiple.splice(findCurrentIndexId, 1)
+        setMultiple(copyMultiple)
+    };
+
+    console.log("selected", multiple)
 
     return (
         <div>
             <AppTitle>Accordian</AppTitle>
 
+            <div className="flex justify-center items-center">
+                <div
+                    onClick={() => setEnableMultiSelect(!enableMultiSelect)}
+                    className={`flex justify-center items-center mb-10 text-xl font-semibold space-x-3 border w-[230px] h-12 text-white  rounded cursor-pointer ${enableMultiSelect ? "bg-blue-500" : "bg-red-500"}`}>
+
+                    <span>{enableMultiSelect ? "Disable Multi Select" : "Enable Multi Select"}</span>
+                    <Power />
+                </div>
+            </div>
+
             <MaxWidth className="w-[500px]">
                 {faqData && faqData.length > 0 ? (
                     faqData.map((data) => (
                         <div
-                            onClick={() => handleSingleClick(data.id)}
+
+                            onClick={
+                                enableMultiSelect
+                                    ? () => handleMultipleSelect(data.id)
+                                    : () => handleSingleClick(data.id)
+                            }
                             key={data.id}
                             className="bg-green-100 my-2 p-4 cursor-pointer">
                             <div className="flex justify-between items-center">
@@ -32,12 +61,12 @@ const Accordian = () => {
                             </div>
 
                             <h3 className="my-3">
-                                {
-                                    selected === data.id ? (
-                                        <p className="bg-white text-lg font-medium p-4 rounded">
-                                            {data.answer}
-                                        </p>
-                                    ) : null
+
+                                {selected === data.id || multiple.includes(data.id) ? (
+                                    <p className="bg-white text-lg font-medium p-4 rounded">
+                                        {data.answer}
+                                    </p>
+                                ) : null
                                 }
                             </h3>
                         </div>
